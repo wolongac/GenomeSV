005d440489f008b066f1ff35c44359e11b05bfb9
wolongac <lhw516@126.com>
2021-06-04 14:35:50 +0800

    add annotation

--- SyRI_Parse.pl
+++ SyRI_Parse.pl
@@ -2,37 +2,35 @@
 
 #In Details :
 #1. Insertion(Trans),Insertion(copygain) and Insertion(Notal) infer the corresponding insertion position  on REF genome using the alignment information.
-#3. For the joining insertion-type SVs,for example: Insertion, Insertion(copygain), Insertion(Notal), and Insertion(Trans), merged into one larger SVs. For TRANS/INVTR, the corresponding Deletion is converted to Deletions.
-
-
+#3. For the contiguous insertion-type SVs,for example: Insertion, Insertion(copygain), Insertion(Notal), and Insertion(Trans), merged into a larger SVs. For TRANS/INVTR, the corresponding Deletions part on ref are converted to Deletions.
 
 #####################Input file format###########################
-#1	chromosome ID in genome A	string
-#2	genome A start position (1-based, includes start position)	int
-#3	genome A end position (1-based, includes end position)	int
-#4	Sequence in genome A (Only for SNPs and indels)	string
-#5	Sequence in genome B (Only for SNPs and indels)	string
-#6	chromosome ID in genome B	string
-#7	genome B start position (1-based, includes start position)	int
-#8	genome B end position (1-based, includes end position)	int
+#1	chromosome ID in genome ref	string
+#2	genome ref start position (1-based, includes start position)	int
+#3	genome ref end position (1-based, includes end position)	int
+#4	Sequence in genome ref (Only for SNPs and indels)	string
+#5	Sequence in genome query (Only for SNPs and indels)	string
+#6	chromosome ID in genome query	string
+#7	genome query start position (1-based, includes start position)	int
+#8	genome query end position (1-based, includes end position)	int
 #9	Unique ID (annotation type + number)	string
-#10	Parent ID (annotation type + number)	string
+#10	Parent ID (annotation type + number)	string  (for structual variation, parent ID  is "-")
 #11	Annotation type	string
 #12	Copy status (for duplications)	string
 #
 ###################################################################
 
 ####################output file format###########################
-#1	chromosome ID in genome A	string
-#2	genome A start position (1-based, includes start position)	int
-#3	genome A end position (1-based, includes end position)	int
-#4	chromosome ID in genome B	string
-#5	genome B start position (1-based, includes start position)	int
-#6	genome B end position (1-based, includes end position)	int
+#1	chromosome ID in genome ref	string
+#2	genome ref start position (1-based, includes start position)	int
+#3	genome ref end position (1-based, includes end position)	int
+#4	chromosome ID in genome query	string
+#5	genome query start position (1-based, includes start position)	int
+#6	genome query end position (1-based, includes end position)	int
 #7	New SV type
-#8	Old SV type
+#8	Raw SV type from SyRI
 #9	Uniq ID
-#10 Parent ID
+#10	Parent ID
 ###################################################################
 
 my $syri_out=shift;
@@ -40,9 +38,26 @@ my $syri_out=shift;
 open(IN,"$syri_out");
 open(OUT,">$syri_out.SV.bed");
 
-my %hash_ref;				#hash_ref{$chr}{$start}{$end};
-my %hash_query;				#$hash_query{$chr}{$start}{$end};
-my %hash_syn_var;			#$hash_syn_var{$sv_name}{'ref'}=$position  or  $hash_syn_var{$sv_name}{'query'}=$position
+my %hash_ref;				#hash_ref{$chr}{$start}{$end}="SV_Type_info"
+##########################################################################
+#The SV_Type_info is :
+#For DUP/INVDP or NOTAL : "DUP_Deletion","NOTAL_Deletion"
+#For INV and SYN : SV_Type_info is "Chr_query\tStart_query\tEnd_query"
+#For TRANS/INVTR : "Struc_Deletion|SV_ID"
+###########################################################################
+
+my %hash_query;				#$hash_query{$chr}{$start}{$end}="SV_Type_info"
+###########################################################################
+#The SV_Type_info is : 
+#For DUP/INVDP and NOTAL : "DUP_Insertion|SV_ID","NOTAL_Insertion|SV_ID"
+#For INV and SYN : SV_Type_info is "Chr_ref\tStart_ref\tEnd_ref"
+#For TRANS/INVTR : "Struc_Insertion|SV_ID"
+############################################################################
+
+my %hash_syn_var;			
+#$hash_syn_var{$sv_name}{'ref'}=$position_on_query
+#$hash_syn_var{$sv_name}{'query'}=$position_on_ref
+#$hash_syn_var{$sv_name}{'info'}="$line[0]\t$line[1]\t$line[2]\t$line[5]\t$line[6]\t$line[7]\t$line[10]\t$line[10]\t$line[8]\t$line[9]";
 my %hash_replacement;  #replacement region : $hash_replacement{'ref'}{$chr}{$start}{$end}  or  $hash_replacement{'query'}{$chr}{$start}{$end}
 my %hash_trans;
 while(<IN>){
@@ -51,13 +66,12 @@ while(<IN>){
 	my @line = split /\s+/,$line;
 	#####################################
 	#SYN INV DUP INVDP TRANS INVTR NOTAL  are Structual Difference(StrucD)
-	#SNP INS DEL CPG CPL HDR TDM are Sequencial Difference(SeqD)
+	#SNP INS DEL CPG CPL HDR TDM are Sequencial Difference(SequentialD)
 	#INVAL INVDPAL INVTRAL SYNAL TRANSAL DUPAL are Alignment(AL)
 	#####################################
 	
-	#Parent exists and not syntenic, remove out
-	#keep syntenic information for the follow analysis
-	
+	#skip SV on the non_synteny regions like : DUP,TRANs or INV et al
+	#Only keep syntenic information for the following analysis
 	next if ($line[9] !~ /SYN/ and $line[9] ne "-");
 	
 	if (($line[10] eq "DUP" or $line[10] eq "INVDP") and $line[11] eq "copyloss"){
@@ -66,7 +80,6 @@ while(<IN>){
 		$hash_ref{$line[0]}{$line[1]}{$line[2]}="DUP_Deletion";
 	}elsif(($line[10] eq "DUP" or $line[10] eq "INVDP") and $line[11] eq "copygain"){
 		#DUP is StrucD,Convert DUP copyloss to Insertion
-		#print OUT "-\t-\t-\t$line[5]\t$line[6]\t$line[7]\tDUP_Insertion\tDUP_copygain\t$line[8]\n";
 		$hash_query{$line[5]}{$line[6]}{$line[7]}="DUP_Insertion|$line[8]";
 	}elsif($line[10] eq "NOTAL" and $line[0] ne "-"){
 		#NOTAL is StrucD,Convert NOTAL of Ref to  Deletion
@@ -74,27 +87,25 @@ while(<IN>){
 		$hash_ref{$line[0]}{$line[1]}{$line[2]}="NOTAL_Deletion";
 	}elsif($line[10] eq "NOTAL" and $line[1] eq "-"){
 		#NOTAL is StrucD,Convert NOTAL of Query to Insertion
-		#print OUT "-\t-\t-\t$line[5]\t$line[6]\t$line[7]\tInsertion\tNOTAL_Insertion\t$line[8]\n";
 		$hash_query{$line[5]}{$line[6]}{$line[7]}="NOTAL_Insertion|$line[8]";
 	}elsif($line[10] eq "CPG" or $line[10] eq "CPL" or $line[10] eq "TDM" or $line[10] eq "HDR"){
-		#CGP,CGL,TDM,HDR of SYN are SeqD, Convert then to Region_replacement 
+		#CGP,CGL,TDM,HDR of SYN are SequentialD, Convert then to Region_replacement 
 		print OUT "$line[0]\t$line[1]\t$line[2]\t$line[5]\t$line[6]\t$line[7]\tRegion_replacement\t$line[10]\t$line[8]\t$line[9]\n";
 		$hash_replacement{'ref'}{$line[0]}{$line[1]}=$line[2];
 		$hash_replacement{'query'}{$line[5]}{$line[6]}=$line[7];
 	}elsif($line[10] eq "INV"){
+		#INV is StrucD,keep the INVs information directly
 		print OUT "$line[0]\t$line[1]\t$line[2]\t$line[5]\t$line[6]\t$line[7]\tRegion_replacement\t$line[10]\t$line[8]\t$line[9]\n";
 		$hash_ref{$line[0]}{$line[1]}{$line[2]}="$line[5]\t$line[6]\t$line[7]";
 		$hash_query{$line[5]}{$line[6]}{$line[7]}="$line[0]\t$line[1]\t$line[2]";
 	}elsif($line[10] eq "TRANS" or $line[10] eq "INVTR"){
 		#Translocation and Inverted Translocation are StrucD, Convert TRANS/INVTR to Deletion and Insertion
-		#print OUT "$line[0]\t$line[1]\t$line[2]\t-\t-\t-\tDeletion\t$line[10]\t$line[8]\t$line[9]\n";
-		
 		$hash_ref{$line[0]}{$line[1]}{$line[2]}="Struc_Deletion|$line[8]";
 		$hash_query{$line[5]}{$line[6]}{$line[7]}="Struc_Insertion|$line[8]";
 		$hash_trans{$line[8]}{"Combine"}="$line[0]\t$line[1]\t$line[2]\t-\t-\t-\tDeletion\t$line[10]\t$line[8]\t$line[9]";
 		$hash_trans{$line[8]}{"No_Combine"}="$line[0]\t$line[1]\t$line[2]\t$line[5]\t$line[6]\t$line[7]\t$line[10]\t$line[10]\t$line[8]\t$line[9]";
 	}elsif($line[10] eq "DEL" or $line[10] eq "INS" or $line[10] eq "SNP"){
-		#DEL,INS,SNP of SYN are SeqD, keep them directly
+		#DEL,INS,SNP of SYN are SequentialD, keep them directly
 		#print OUT "$line[0]\t$line[1]\t$line[2]\t$line[5]\t$line[6]\t$line[7]\t$line[10]\t$line[10]\t$line[8]\n";
 		$hash_syn_var{$line[8]}{'ref'}="$line[0]\t$line[1]\t$line[2]";
 		$hash_syn_var{$line[8]}{'query'}="$line[5]\t$line[6]\t$line[7]";
@@ -201,6 +212,8 @@ foreach my $trans (keys %hash_trans){
 		print OUT "$hash_trans{$trans}{'No_Combine'}\n";
 	}
 }
+
+#output sequencial differences:
 foreach my $sv (keys %hash_syn_var){
 	my @ref=split /\t/,$hash_syn_var{$sv}{'ref'};
 	my @query=split /\t/,$hash_syn_var{$sv}{'query'};
